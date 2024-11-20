964c497d801e23b7de43fe10ec8fee54ab697be8
Aliya Nurbekova <aliyanura9@gmail.com>
2024-11-20 17:19:32 +0600

    create product app

---
+++ product/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from seller.models import Seller
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_name = 'categories'
+        verbos_name = 'Категория'
+        verbos_name_plural = 'Категории'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                verbose_name='Цена')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE,
+                               related_name='products',
+                               verbose_name='Продавец')
+    categories = models.ManyToManyField(Category, related_name='products',
+                                        verbose_name='Категории')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_name = 'products'
+        verbos_name = 'Продукт'
+        verbos_name_plural = 'Продукты'
