9af2008c6a426a2569bc2ea6f53af4e2882bee9e
Aliya Nurbekova <aliyanura9@gmail.com>
2024-11-21 10:36:03 +0600

    fix swagger

--- shop_project/urls.py
+++ shop_project/urls.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 from seller.views import notifications_view
-from rest_framework.routers import DefaultRouter
 
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Test API",
+        title="Your Project API",
         default_version='v1',
+        description="Documentation for the API",
     ),
     public=True,
+    permission_classes=[AllowAny], 
 )
 
 
